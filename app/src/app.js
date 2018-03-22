35a1b1671888787c9e1d11f936404e8643da7f65
raksit <rak-sit@hotmail.com>
2018-03-22 11:14:23 +0700

    first commit

---
+++ app/src/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+})
+
+const db = require('./db')
+const port = process.env.PORT || 8080
+const VERSION = process.env.VERSION || '1'
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send("Match service is running...")
+})
+
+let server = app.listen(port, () => {
+    let port = server.address().port
+    console.log(`jes-match-service version : ${VERSION}`)
+    console.log('running at http://' + 'localhost' + ':' + port)
+})
+
+module.exports = server
